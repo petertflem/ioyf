c26aedb348338c0670c2cc3c29aa3f614914256e
petfle <peter.tollnes.flem@knowit.no>
2015-06-17 14:28:03 +0200

    Added back the settings plugin. Now lists out the featured image on the front page for every blog post.

--- wp-ioyf/wp-content/themes/ioyf/functions.php
+++ wp-ioyf/wp-content/themes/ioyf/functions.php
@@ -2,12 +2,6 @@
 
 define('ROOT', get_stylesheet_directory_uri());
 
-function load_scripts() {
-  wp_register_script( 'google-map', 'https://maps.googleapis.com/maps/api/js', array(), '1.0.0', true);
-  wp_register_script( 'init-map', get_template_directory_uri() . '/assets/js/init-map.js', array('google-map'), '1.0.0', true);
-  wp_enqueue_script('google-map');
-  wp_enqueue_script('init-map');
-}
-add_action( 'wp_enqueue_scripts', 'load_scripts' );
+add_theme_support( 'post-thumbnails' );
 
 ?>
--- wp-ioyf/wp-content/themes/ioyf/index.php
+++ wp-ioyf/wp-content/themes/ioyf/index.php
@@ -1,72 +1,34 @@
+<?php $options = get_option('ioyf_settings'); ?>
+
 <?php get_header(); ?>
 
-<?php
-  $options = get_option('ioyf_settings');
-?>
-
-    <div class="container">
-      <div class="row">
-        <div class="col-xs-12">
-          <img src="<?php echo ROOT; ?>/assets/images/oks-logo.png" class="oks-logo img-responsive" alt="oks logo" />
-        </div>
-      </div>
-      <div class="row">
-        <div class="col-xs-12 top-banner">
-          <span class="ynf-subtitle"><?php echo $options['ioyf_logo_subtitle']; ?> </span>
-          <span class="top-banner-date-and-time"><?php echo $options['ioyf_top_banner_date_and_time']; ?></span>
-        </div>
-      </div>
-      <div class="row">
-        <div class="col-xs-12 top-information">
-          <?php echo apply_filters('the_content', $options['top_information']); ?>
-        </div>
-      </div>
-      <div class="row">
-        <div class="col-xs-12 young-and-free-banner">
-          <div class="young-and-free-banner-container">
-          </div>
-        </div>
-      </div>
-      <div class="row">
-        <div class="col-xs-12">
-          <div class="time-information">
-            <img src="<?php echo ROOT; ?>/assets/images/watch.png" class="img-responsive" alt="bilde av en klokke" />
-            <p><?php echo $options['duration_information']; ?></p>
-          </div>
-        </div>
-      </div>
-      <div class="row">
-        <div class="col-xs-12 freedom-background-color">
-          <div class="freedom-container">
-            <div class="freedom">
-              <span class="freedom-title"><?php echo $options['freedom_title']; ?></span>
-              <br />
-              <br />
-              <br />
-              <span><?php echo apply_filters('the_content', $options['freedom_text']); ?></span>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="row">
-        <div class="col-xs-12 bottom-information">
-          <?php echo apply_filters('the_content', $options['bottom_information']); ?>
-        </div>
-      </div>
-      <div class="row map-row">
-        <div class="col-cs-12">
-          <div id="map"></div>
-        </div>
-      </div>
-      <div class="row">
-        <div class="col-xs-12 facebook">
-          <div class="facebook-container">
-            <a href="<?php echo $options['ioyf_facebook_banner_link']; ?>">
-              <img src="<?php echo ROOT; ?>/assets/images/facebook-logo.png" class="img-responsive facebook-logo" alt="facebook logo" />
-              <span><?php echo $options['ioyf_facebook_banner_text']; ?></span>
-            </a>
-          </div>
-        </div>
+<div class="container">
+  <div class="row">
+    <div class="col-xs-12">
+      <img src="<?php echo ROOT; ?>/assets/images/oks-logo.png" class="oks-logo img-responsive" alt="oks logo" />
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-xs-12 top-banner">
+      <span class="ynf-subtitle"><?php echo $options['ioyf_logo_subtitle']; ?> </span>
+      <span class="top-banner-date-and-time"><?php echo $options['ioyf_top_banner_date_and_time']; ?></span>
+    </div>
+  </div>
+  
+  <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+    <div class="row">
+      <div class="col-xs-12">
+        <?php the_post_thumbnail(); ?>
       </div>
     </div>
+  <?php endwhile; else : ?>
+    <div class="row">
+      <div class="col-xs-12">
+        <?php _e( 'Sorry, no posts matched your criteria.' ); ?>
+      </div>
+    </div>
+  <?php endif; ?>
+  
+</div>
+
 <?php get_footer(); ?>
